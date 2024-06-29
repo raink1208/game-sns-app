22d30d87b8c901dcb1ea3edf1f5ccff292249e3b
raink1208 <58714901+raink1208@users.noreply.github.com>
2024-06-30 02:47:01 +0900

    UniqueIdでの検索を追加

--- game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/infrastructure/repository/user/IUserRepository.kt
+++ game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/infrastructure/repository/user/IUserRepository.kt
@@ -4,10 +4,12 @@ import com.github.raink1208.gamesnsapp.server.application.dto.UserDTO
 import com.github.raink1208.gamesnsapp.server.domain.model.User
 import com.github.raink1208.gamesnsapp.server.domain.valueobject.UserId
 import com.github.raink1208.gamesnsapp.server.domain.valueobject.UserName
+import com.github.raink1208.gamesnsapp.server.domain.valueobject.UserUniqueId
 
 interface IUserRepository {
     fun register(user: User)
     fun save(user: User)
+    fun findByUniqueId(uniqueId: UserUniqueId): UserDTO?
     fun findById(userId: UserId): UserDTO?
     fun findByUserName(userName: UserName): List<UserDTO>
 }
