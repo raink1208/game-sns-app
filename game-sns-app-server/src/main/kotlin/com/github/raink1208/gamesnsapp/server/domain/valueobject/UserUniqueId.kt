10c3e2449035289b2950b255c2f24f33c6224bbb
raink1208 <58714901+raink1208@users.noreply.github.com>
2024-06-29 22:10:45 +0900

    ULIDの生成をUserUniqueIdに追加

--- game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/domain/valueobject/UserUniqueId.kt
+++ game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/domain/valueobject/UserUniqueId.kt
@@ -5,6 +5,12 @@ import de.huxhorn.sulky.ulid.ULID
 data class UserUniqueId(
     val uniqueId: ULID.Value
 ) {
+    companion object {
+        fun newId(): UserUniqueId {
+            return UserUniqueId(ULID().nextValue())
+        }
+    }
+
     override fun equals(other: Any?): Boolean {
         if (other !is UserUniqueId) return false
 
