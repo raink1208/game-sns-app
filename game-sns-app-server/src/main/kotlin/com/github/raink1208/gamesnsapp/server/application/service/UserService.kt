b349b7cea3e266ae823505ca3abfa721022dd14b
raink1208 <58714901+raink1208@users.noreply.github.com>
2024-06-29 22:11:26 +0900

    User検索に関するメソッドを追加

--- game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/application/service/UserService.kt
+++ game-sns-app-server/src/main/kotlin/com/github/raink1208/gamesnsapp/server/application/service/UserService.kt
@@ -1,19 +1,32 @@
 package com.github.raink1208.gamesnsapp.server.application.service
 
+import com.github.raink1208.gamesnsapp.server.application.exception.UserNotFoundException
 import com.github.raink1208.gamesnsapp.server.domain.factory.UserFactory
 import com.github.raink1208.gamesnsapp.server.domain.model.User
-import com.github.raink1208.gamesnsapp.server.domain.repository.IUserRepository
+import com.github.raink1208.gamesnsapp.server.infrastructure.repository.user.IUserRepository
 import com.github.raink1208.gamesnsapp.server.domain.valueobject.UserId
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
-class UserService constructor (
+class UserService (
     private val userRepository: IUserRepository,
     private val userFactory: UserFactory
-) {
-    fun findUserById(userId: String): User {
-        val userDto = userRepository.findById(UserId(userId))
+): IUserService {
+    private val logger = LoggerFactory.getLogger(UserService::class.java)
+
+    override fun findUserByUniqueId(uniqueId: String): User? {
+        TODO("Not yet implemented")
+    }
+
+    override fun findUserById(userId: String): User? {
+        val userDto = userRepository.findById(UserId(userId)) ?:
+            throw UserNotFoundException("User not found By UserId $userId")
         val user = userFactory.createUser(userDto)
         return user
+    }
+
+    override fun findUsersByName(userName: String): List<User> {
+        TODO("Not yet implemented")
     }
 }
